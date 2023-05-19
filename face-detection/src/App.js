0931102ef5cd990d689161d3c3b9eee8e6bf46fa
Ladis Nelissen <ladisnelissen@hotmail.com>
2023-05-19 19:52:06 +0200

    feat: create face mesh

--- face-detection/src/App.js
+++ face-detection/src/App.js
@@ -1,12 +1,87 @@
+import React, {useRef} from 'react';
 import Webcam from 'react-webcam';
-
-import './App.css';
+import * as tf from '@tensorflow/tfjs';
+import * as facemesh from '@tensorflow-models/facemesh';
+import draw, { drawMesh } from './utilities'
 
 function App() {
-  return (
-    <div className="App">
-      <Webcam />
-    </div>
-  );
+  const webcamRef = useRef(null);
+  const canvasRef = useRef(null);
+   
+
+  const runFacemesh = async () => {
+    const net = await facemesh.load({
+      inputResolution: {width:640, height:480}, scale:0.8,
+
+    });
+    setInterval(() => {
+      detect(net);
+    }, 100);
+  };
+
+  const detect = async (net) => {
+    if(typeof webcamRef.current !== "undefined" && webcamRef.current !== null && webcamRef.current.video.readyState === 4){
+      // Get Video Properties
+      const video = webcamRef.current.video;
+      const videoWidth = webcamRef.current.video.videoWidth;
+      const videoHeight = webcamRef.current.video.videoHeight;
+
+      // Set video width
+      webcamRef.current.video.width = videoWidth;
+      webcamRef.current.video.height = videoHeight;
+
+      // Set canvas width
+      canvasRef.current.width = videoWidth;
+      canvasRef.current.height = videoHeight;
+
+      // Make Detections
+      const face = await net.estimateFaces(video);
+      console.log(face);
+
+      const ctx = canvasRef.current.getContext("2d");
+      drawMesh(face, ctx);
+    }
+  };
+
+  runFacemesh();
+
+   return (
+     <div className="App">
+       <header className="App-header">
+         <Webcam
+          ref={webcamRef}
+          style={{
+            position: "absolute",
+            marginLeft: "auto",
+            marginRight: "auto",
+            top:100,
+            left:0,
+            right:80,
+            textAlign: "center",
+            zIndex:9,
+            width:640,
+            height:480,
+         }}
+          />
+ 
+         <canvas
+          ref={canvasRef}
+          style={{
+           position: "absolute",
+           marginLeft: "auto",
+           marginRight: "auto",
+           top:100,
+           left:0,
+           right:80,
+           textAlign: "center",
+           zIndex:9,
+           width:640,
+           height:480,
+        }}
+         />    
+       </header>
+     </div>
+   );
+ 
 }
 export default App;
