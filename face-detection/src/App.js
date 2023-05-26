1afd1b16be1bbe23cd4fb1ca2bec1605a17efbcd
Ladis Nelissen <ladisnelissen@hotmail.com>
2023-05-26 15:19:14 +0200

    feat: setup video and load models

--- face-detection/src/App.js
+++ face-detection/src/App.js
@@ -1,11 +1,38 @@
-import React, {useRef} from 'react';
+import React, {useRef, useState, useEffect} from 'react';
 import Webcam from 'react-webcam';
 import * as faceapi from 'face-api.js';
 
 
-
 function App() {
-  
+  const videoHeight = 480;
+  const videoWidth = 640;
+  const [initializing, setInitializing] = useState(false);
+  const webcamRef = useRef();
+  const canvasRef = useRef();
  
+  useEffect(() => {
+    const loadModels = async () => {
+      const MODEL_URL = process.env.PUBLIC_URL + '/models';
+      setInitializing(true);
+      Promise.all([
+        faceapi.nets.tinyFaceDetector.loadFromUri(MODEL_URL),
+        faceapi.nets.faceLandmark68Net.loadFromUri(MODEL_URL),
+        faceapi.nets.faceRecognitionNet.loadFromUri(MODEL_URL),
+        faceapi.nets.faceExpressionNet.loadFromUri(MODEL_URL),
+      ]).then(startVideo);
+    }
+    loadModels();
+  }
+  ,[])
+
+  const startVideo = () => {
+    navigator.getUserMedia(
+      { video: {} },
+      stream => (webcamRef.current.srcObject = stream)
+
+    );
+  }
+
+    
 }
 export default App;
